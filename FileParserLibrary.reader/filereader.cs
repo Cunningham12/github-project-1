1c84c605a1574211f5aa2a6c02a671ec2d91f54b
Cunningham12 <119353506+Cunningham12@users.noreply.github.com>
2025-03-03 14:48:04 -0500

    Create filereader.cs

---
+++ FileParserLibrary.reader/filereader.cs
@@ -0,0 +1,27 @@
+
+namespace FileReaderParserLibrary;
+
+using System;
+using System.IO;
+
+/// <summary>
+/// A class for reading content from a file.
+/// </summary>
+public class FileReader
+{
+    /// <summary>
+    /// Reads the entire content of the specified file.
+    /// </summary>
+    /// <param name="filePath">The path of the file to read.</param>
+    /// <returns>A string containing the content of the file.</returns>
+    /// <exception cref="FileNotFoundException">Thrown if the specified file is not found.</exception>
+    public string ReadFile(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("The specified file was not found.", filePath);
+        }
+
+        return File.ReadAllText(filePath);
+    }
+}
